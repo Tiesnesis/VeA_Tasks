e1259f61afede5d210c486d0aa73ab7900d5064a
Tiesnesis <ulvisgoldbergs@gmail.com>
2016-05-10 02:14:32 +0300

    Add gaming and player threads and game logic

--- quizDown/QuizDownServer/QuizDownServer/Game.cs
+++ quizDown/QuizDownServer/QuizDownServer/Game.cs
@@ -2,21 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace QuizDownServer
 {
     class Game
     {
-        Player player_1;
-        Player player_2;
-        List<Question> questions;
-        int currentQuestion;
-        Network.NetworkTcpServer tcpServer;
+        NetworkTcpServer tcpServer;
 
-        public void selectQuestions()
+        private List<Question> selectQuestions()
         {
+            List<Question> questions = new List<Question>();
             Random rnd = new Random();
             Database temp_connection = new Database();
             List<Question> temp_list = temp_connection.Select();
@@ -26,19 +22,68 @@ namespace QuizDownServer
                 questions.Add(temp_list.ElementAt(index));
                 temp_list.RemoveAt(index);
             }
+            return questions;
         }
 
         public void startServerConenction()
         {
-            tcpServer = new Network.NetworkTcpServer("localhost", 4567);
+            tcpServer = new NetworkTcpServer("localhost", 4567);
         }
 
-        public void waitForPlayers()
+        private void doGame()
         {
-            NetworkTcpServer.Connection connection =
-                      tcpServer.waitForNewConnection();
-//TODO Client side PLayer decides how many score it gets
-//TODO  
+            List<Question> questions = selectQuestions();
+            Player player_1 = new Player(tcpServer.waitForNewConnection(), questions);
+            Player player_2 = new Player(tcpServer.waitForNewConnection(), questions);
+
+            player_1.status = "sendQuestions";
+            player_2.status = "sendQuestions";
+            while(player_1.status != "wait" && player_2.status != "wait")
+            {
+
+            }
+            int i = 0;
+            do
+            {
+
+                i++;
+                player_1.status = i.ToString();
+                player_2.status = i.ToString();
+                while (player_1.status != "wait" && player_2.status != "wait")
+                {
+
+                }
+                player_1.opScore = player_2.score;
+                player_2.opScore = player_1.score;
+                player_1.status = "sendScore";
+                player_2.status = "sendScore";
+                while (player_1.status != "wait" && player_2.status != "wait")
+                {
+
+                }
+            } while (i < 7);
+            player_1.status = "end";
+            player_2.status = "end";
         }
+
+        public void playGame()
+        {
+            while (true)
+            {
+                ThreadStart game_ref = new ThreadStart(doGame);
+                Thread game_thread = new Thread(game_ref);
+                game_thread.Start();
+            }
+            
+        }
+
+        public void launchGamingThread()
+        {
+            ThreadStart launch_ref = new ThreadStart(playGame);
+            Thread launch_thread = new Thread(launch_ref);
+            launch_thread.Start();
+        }
+
+
     }
 }
--- quizDown/QuizDownServer/QuizDownServer/Player.cs
+++ quizDown/QuizDownServer/QuizDownServer/Player.cs
@@ -13,15 +13,27 @@ namespace QuizDownServer
     class Player
     {
         NetworkTcpServer.Connection connection;
+        public int score, opScore;
+        public string status;
+        public List<Question> question_list;
 
+        public Player(NetworkTcpServer.Connection getConnection, List<Question> q_list)
+        {
+            connection = getConnection;
+            score = 0;
+            opScore = 0;
+            question_list = q_list;
+
+
+        }
         public void sendQuestions(List<Question> question_list)
         {
             connection.send(question_list);
         }
 
-        public int receiveScore()
+        public void receiveScore()
         {
-            return int.Parse(connection.receive().ToString());
+            score = int.Parse(connection.receive().ToString());
         }
 
         public void sendOponentScore(int op_score)
@@ -35,6 +47,65 @@ namespace QuizDownServer
 
         }
 
+        private void do_game()
+        {
+            while (status != "end")
+            {
+                switch (status)
+                {
+                    case "sendQuestions":
+                        sendQuestions(question_list);
+                        status = "wait";
+                        break;
+                    case "1":
+                        sendCurrentQuestion(1);
+                        status = "getScore";
+                        break;
+                    case "2":
+                        sendCurrentQuestion(2);
+                        status = "getScore";
+                        break;
+                    case "3":
+                        sendCurrentQuestion(3);
+                        status = "getScore";
+                        break;
+                    case "4":
+                        sendCurrentQuestion(4);
+                        status = "getScore";
+                        break;
+                    case "5":
+                        sendCurrentQuestion(5);
+                        status = "getScore";
+                        break;
+                    case "6":
+                        sendCurrentQuestion(6);
+                        status = "getScore";
+                        break;
+                    case "7":
+                        sendCurrentQuestion(7);
+                        status = "getScore";
+                        break;
+                    case "getScore":
+                        receiveScore();
+                        status = "wait";
+                        break;
+                    case "sendScore":
+                        sendOponentScore(opScore);
+                        status = "wait";
+                        break;
+                    case "wait":
+                        break;
+                }
+            }
+            close();
+        }
+
+        public void playGame()
+        {
+            ThreadStart player_ref = new ThreadStart(do_game);
+            Thread player_thread = new Thread(player_ref);
+            player_thread.Start();
+        }
         public void close()
         {
             connection.closeConnection();
